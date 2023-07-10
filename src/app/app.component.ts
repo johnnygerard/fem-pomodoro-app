fe6c161011a9a42dc126e555e3f24504d5faefa0
Johnny Gérard <john@jgerard.dev>
2023-07-10 21:48:22 +0200

    feat: SVG templates (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from 'src/environments/environment';
+import { CheckmarkSvgComponent } from './svg/checkmark-svg.component';
+import { CrossSvgComponent } from './svg/cross-svg.component';
+import { DownArrowSvgComponent } from './svg/down-arrow-svg.component';
+import { GearSvgComponent } from './svg/gear-svg.component';
+import { UpArrowSvgComponent } from './svg/up-arrow-svg.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CheckmarkSvgComponent,
+    CrossSvgComponent,
+    DownArrowSvgComponent,
+    GearSvgComponent,
+    UpArrowSvgComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
