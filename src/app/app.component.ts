a32022cd6dccd05555cfb80531512a41cb75a128
Johnny Gérard <john@jgerard.dev>
2023-07-13 12:04:13 +0200

    feat: settings component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { environment } from 'src/environments/environment';
-import { TimerComponent } from './timer/timer.component';
+import { SettingsComponent } from './settings/settings.component';
+import { Key } from './key.enum';
+import { Font } from './font.enum';
+import { Color } from './color.enum';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    TimerComponent,
+    SettingsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    if (environment.production) {
-      console.log(`Challenge by Frontend Mentor. Coded by Johnny Gérard.
+export class AppComponent {
+  constructor(renderer: Renderer2) {
+    // Initialize color and font themes.
+    const fontTheme = window.localStorage.getItem(Key.FONT) ?? Font.SANS_SERIF;
+    const colorTheme = window.localStorage.getItem(Key.COLOR) ?? Color.CORAL_PINK;
 
-Portfolio: https://www.frontendmentor.io/profile/johnnygerard`);
-    }
+    renderer.setAttribute(window.document.body, Key.FONT, fontTheme.toString());
+    renderer.setAttribute(window.document.body, Key.COLOR, colorTheme.toString());
   }
 }
---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,90 @@
+import { ChangeDetectorRef, Component, EventEmitter, Output, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CrossSvgComponent } from '../svg/cross-svg.component';
+import { CheckmarkSvgComponent } from '../svg/checkmark-svg.component';
+import { UpArrowSvgComponent } from '../svg/up-arrow-svg.component';
+import { DownArrowSvgComponent } from '../svg/down-arrow-svg.component';
+import { Color } from '../color.enum';
+import { Key } from '../key.enum';
+import { Font } from '../font.enum';
+import { FormsModule } from '@angular/forms';
+import { TimerService } from '../timer.service';
+import { TimerType } from '../timer-type.enum';
+
+const body = window.document.body;
+
+type TimerSettings = {
+  timeSpan: number,
+  label: string,
+}
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    CrossSvgComponent,
+    CheckmarkSvgComponent,
+    UpArrowSvgComponent,
+    DownArrowSvgComponent,
+  ],
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent {
+  protected colorTheme: Color;
+  protected fontTheme: Font;
+  protected pomodoroTimeSpan: number;
+  protected shortBreakTimeSpan: number;
+  protected longBreakTimeSpan: number;
+
+  @Output() closeDialogEvent = new EventEmitter<void>();
+
+  constructor(
+    private readonly renderer: Renderer2,
+    private readonly timerService: TimerService,
+  ) {
+    // Initialize color and font themes
+    const colorTheme = body.getAttribute(Key.COLOR);
+    const fontTheme = body.getAttribute(Key.FONT);
+
+    if (colorTheme === null || fontTheme === null)
+      throw Error('Missing one or more theme attributes');
+    this.colorTheme = +colorTheme;
+    this.fontTheme = +fontTheme;
+
+    // Initialize timer settings
+    this.pomodoroTimeSpan = this.timerService.pomodoroTime;
+    this.shortBreakTimeSpan = this.timerService.shortBreakTime;
+    this.longBreakTimeSpan = this.timerService.longBreakTime;
+  }
+
+  protected closeDialog(): void {
+    this.closeDialogEvent.emit();
+  }
+
+
+  protected stepUp(input: HTMLInputElement): void {
+    input.stepUp();
+
+    // Notify ngModel of the update
+    input.dispatchEvent(new Event('input'));
+  }
+
+  protected stepDown(input: HTMLInputElement): void {
+    input.stepDown();
+
+    // Notify ngModel of the update
+    input.dispatchEvent(new Event('input'));
+  }
+
+  protected applySettings(): void {
+    this.renderer.setAttribute(body, Key.COLOR, this.colorTheme.toString());
+    this.renderer.setAttribute(body, Key.FONT, this.fontTheme.toString());
+    this.timerService.pomodoroTime = this.pomodoroTimeSpan;
+    this.timerService.shortBreakTime = this.shortBreakTimeSpan;
+    this.timerService.longBreakTime = this.longBreakTimeSpan;
+    this.closeDialog();
+  }
+}
